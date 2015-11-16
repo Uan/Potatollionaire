334794a70dc520806ee4117e2e57e5f447260a00
IEACATADB <nyacromancer@gmail.com>
2015-11-16 09:25:48 +0300

    ruby script to add script paths

---
+++ ruby/test.rb
@@ -0,0 +1,18 @@
+def replace(file_path, contents)
+  file = File.open(file_path, 'r+')
+  html = ''
+  html += file.readline until file.eof?
+  file.close
+  html.gsub(/<!--start-->(.*)<!--end-->/im, contents)
+end
+paths = Dir['../js/*']
+stuff = "<!--start-->\n"
+paths.each do |i|
+  stuff += "<script type='text/javascript' src='#{i}'></script>\n"
+end
+stuff += "<!--end-->\n"
+IO.write('../index.html', File.open('../index.html') do |_f|
+  replace('../index.html', stuff)
+end
+        )
+        
